db8f8b485e373875730ae922fdbce704fe8b5bc9
ponomarevda <ponomarevda96@gmail.com>
2019-10-09 21:12:58 +0300

    [GUI] fix displayed numbers and array indexes and some optimization

--- gui.py
+++ gui.py
@@ -20,19 +20,33 @@ class MainWindow(QMainWindow):
         self.CELL_SIZE = 30
         self.EDGE_SIZE = 10
 
-
-        self.buttons = list()
-        for row in range(0, self.SIZE_X + 1, 1):
-            self.buttons.append(list())
+        self.cells = list()
+        self.horizontalEdge = list()
+        self.verticalEdge = list()
+        for row in range(self.SIZE_Y,  -1, -1):
             if (row % 2) == 0:
-                for col in range(0, self.SIZE_Y, 2):
-                    self.addEmptySpace(layout, row, col)
-                    self.addHorizontalEdge(layout, row, col + 1)
+                heRow = int((self.SIZE_Y - row)/2)  # from 0 to 9
+                print(heRow)
+                self.horizontalEdge.append(list())
+                print(heRow)
+                for col in range(0, self.SIZE_X, 2):
+                    heCol = int(col/2) # from 0 to 8
+                    self.addHorizontalEdge(heRow, heCol)
+                    layout.addWidget(self.horizontalEdge[heRow][heCol], row, col + 1, 1, 1)
             else:
+                veRow = int((self.SIZE_Y - row)/2) # from 0 to 8
+                cellRow = int((self.SIZE_Y - row)/2) # from 0 to 8
+                self.verticalEdge.append(list())
+                self.cells.append(list())
                 for col in range(0, self.SIZE_Y, 2):
-                    self.addVerticalEdge(layout, row, col)
-                    self.addCell(layout, row, col + 1)
-                self.addVerticalEdge(layout, row, col + 2)
+                    veCol = int(col/2) # from 0 to 9
+                    cellCol = int(col/2) # from 0 to 8
+                    self.addVerticalEdge(veRow, veCol)
+                    self.addCell(cellRow, cellCol)
+                    layout.addWidget(self.verticalEdge[veRow][veCol], row, col, 1, 1)
+                    layout.addWidget(self.cells[cellRow][cellCol], row, col + 1, 1, 1)
+                self.addVerticalEdge(veRow, veCol + 1)
+                layout.addWidget(self.verticalEdge[veRow][veCol + 1], row, col + 2, 1, 1)
 
 
         widget = QWidget()
@@ -45,41 +59,37 @@ class MainWindow(QMainWindow):
         self.setGeometry(window_pose_x, window_pose_y, window_size_x, window_size_y)
         self.setCentralWidget(widget)
 
-    def addCell(self, layout, row, col):
-        self.buttons[row].append( QPushButton( str(int(1 + self.CELLS_AMOUNT_X - (row + 1)/2)) + "/" + str(int((col + 1)/2)) ))
-        self.buttons[row][col].setMinimumSize(QSize(self.CELL_SIZE, self.CELL_SIZE))
-        self.buttons[row][col].pressed.connect( lambda r=row, c=col: self.func(r, c))
-        self.buttons[row][col].setStyleSheet("QPushButton {background-color: #CFFFFF}") #A3C1DA}")
-        layout.addWidget(self.buttons[row][col], row, col, 1, 1)
+    def addCell(self, row, col):
+        self.cells[row].append( QPushButton(str(col) + "/" + str(row)) )
+        self.cells[row][col].setMinimumSize(QSize(self.CELL_SIZE, self.CELL_SIZE))
+        self.cells[row][col].pressed.connect( lambda r=row, c=col: self.cellCallback(r, c))
+        self.cells[row][col].setStyleSheet("QPushButton {background-color: #CFFFFF}")
 
-    def addVerticalEdge(self, layout, row, col):
-        self.buttons[row].append( QPushButton("k") )
-        self.buttons[row][col].setMinimumSize(QSize(self.EDGE_SIZE, self.CELL_SIZE))
-        self.buttons[row][col].setMaximumSize(QSize(self.EDGE_SIZE, self.CELL_SIZE))
-        self.buttons[row][col].pressed.connect( lambda r=row, c=col: self.func(r, c))
-        self.buttons[row][col].setStyleSheet("QPushButton {background-color: #FFFFFF}") #A3C1DA}")
-        layout.addWidget(self.buttons[row][col], row, col, 1, 1)
+    def addVerticalEdge(self, row, col):
+        self.verticalEdge[row].append( QPushButton("v") )
+        self.verticalEdge[row][col].setMinimumSize(QSize(self.EDGE_SIZE, self.CELL_SIZE))
+        self.verticalEdge[row][col].setMaximumSize(QSize(self.EDGE_SIZE, self.CELL_SIZE))
+        self.verticalEdge[row][col].pressed.connect( lambda r=row, c=col: self.verticalEdgeCallback(r, c))
+        self.verticalEdge[row][col].setStyleSheet("QPushButton {background-color: #FFFFFF}")
 
-    def addHorizontalEdge(self, layout, row, col):
-        self.buttons[row].append( QPushButton("k") )
-        self.buttons[row][col].setMinimumSize(QSize(self.CELL_SIZE, self.EDGE_SIZE))
-        self.buttons[row][col].setMaximumSize(QSize(self.CELL_SIZE, self.EDGE_SIZE))
-        self.buttons[row][col].pressed.connect( lambda r=row, c=col: self.func(r, c))
-        self.buttons[row][col].setStyleSheet("QPushButton {background-color: #FFFFFF}") #A3C1DA}")
-        layout.addWidget(self.buttons[row][col], row, col, 1, 1)
+    def addHorizontalEdge(self, row, col):
+        self.horizontalEdge[row].append( QPushButton("h") )
+        self.horizontalEdge[row][col].setMinimumSize(QSize(self.CELL_SIZE, self.EDGE_SIZE))
+        self.horizontalEdge[row][col].setMaximumSize(QSize(self.CELL_SIZE, self.EDGE_SIZE))
+        self.horizontalEdge[row][col].pressed.connect( lambda r=row, c=col: self.horizontalEdgeCallback(r, c))
+        self.horizontalEdge[row][col].setStyleSheet("QPushButton {background-color: #FFFFFF}")
 
-    def addEmptySpace(self, layout, row, col):
-        self.buttons[row].append( QLabel() )
-        self.buttons[row][col].setMinimumSize(QSize(self.EDGE_SIZE, self.EDGE_SIZE))
-        self.buttons[row][col].setMaximumSize(QSize(self.EDGE_SIZE, self.EDGE_SIZE))
-        #self.buttons[row][col].pressed.connect( lambda r=row, c=col: self.func(r, c))
-        #self.buttons[row][col].setStyleSheet("QPushButton {background-color: #FFFFFF}") #A3C1DA}")
-        layout.addWidget(self.buttons[row][col], row, col, 1, 1)
+    def cellCallback(self, row, col):
+        self.cells[row][col].setStyleSheet("QPushButton {background-color: #F00000}")
+        print(str(col) + "/" + str(row))
 
-    def func(self, row, col):
-        self.buttons[row][col].setStyleSheet("QPushButton {background-color: #03C1DA}")
-        #self.setStyleSheet("QPushButton {background-color: #03000A}")
-        print(str(col + 1) + "/" + str(self.CELLS_AMOUNT_X - row))
+    def verticalEdgeCallback(self, row, col):
+        self.verticalEdge[row][col].setStyleSheet("QPushButton {background-color: #00F000}")
+        print(str(col) + "/" + str(row))
+
+    def horizontalEdgeCallback(self, row, col):
+        self.horizontalEdge[row][col].setStyleSheet("QPushButton {background-color: #0000F0}")
+        print(str(col) + "/" + str(row))
 
 app = QApplication(sys.argv)
 label = QLabel('World creator v. 0.01')
